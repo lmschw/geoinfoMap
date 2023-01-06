86789420d2073e68d214003cd48fd7ec6d9415f2
Lilly Schwarzenbach <lillyschwarzenbach@gmail.com>
2023-01-06 19:15:59 +0100

    add first icon to map

--- geoinfo-map/main.js
+++ geoinfo-map/main.js
@@ -1,17 +1,91 @@
-import './style.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
+import Feature from 'ol/Feature.js';
+import Map from 'ol/Map.js';
+import Overlay from 'ol/Overlay.js';
+import Point from 'ol/geom/Point.js';
+import TileJSON from 'ol/source/TileJSON.js';
+import VectorSource from 'ol/source/Vector.js';
+import View from 'ol/View.js';
+import {Icon, Style} from 'ol/style.js';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
 import OSM from 'ol/source/OSM';
 
-const map = new Map({
-  target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM()
-    })
-  ],
-  view: new View({
-    center: [0, 0],
-    zoom: 2
-  })
+const sachselnCoords = [917600, 5921700]
+
+const iconFeature = new Feature({
+  geometry: new Point(sachselnCoords),
+  name: 'Sachseln',
 });
+
+const iconStyle = new Style({
+  image: new Icon({
+    anchor: [0.5, 46],
+    anchorXUnits: 'fraction',
+    anchorYUnits: 'pixels',
+    src: 'arrow_down.png',
+    scale: 0.25,
+  }),
+});
+
+iconFeature.setStyle(iconStyle);
+
+const vectorSource = new VectorSource({
+  features: [iconFeature],
+});
+
+const vectorLayer = new VectorLayer({
+  source: vectorSource,
+});
+
+const map = new Map({
+  layers: [    new TileLayer({
+    source: new OSM()
+  }), vectorLayer],
+  target: document.getElementById('map'),
+  view: new View({
+    center: sachselnCoords,
+    zoom: 9,
+  }),
+});
+
+const element = document.getElementById('popup');
+
+const popup = new Overlay({
+  element: element,
+  positioning: 'bottom-center',
+  stopEvent: false,
+});
+map.addOverlay(popup);
+
+let popover;
+function disposePopover() {
+  if (popover) {
+    popover.dispose();
+    popover = undefined;
+  }
+}
+// display popup on click
+map.on('click', function (evt) {
+  const feature = map.forEachFeatureAtPixel(evt.pixel, function (feature) {
+    return feature;
+  });
+  disposePopover();
+  if (!feature) {
+    return;
+  }
+  popup.setPosition(evt.coordinate);
+  popover = new bootstrap.Popover(element, {
+    placement: 'top',
+    html: true,
+    content: feature.get('name'),
+  });
+  popover.show();
+});
+
+// change mouse cursor when over marker
+map.on('pointermove', function (e) {
+  const pixel = map.getEventPixel(e.originalEvent);
+  const hit = map.hasFeatureAtPixel(pixel);
+  map.getTarget().style.cursor = hit ? 'pointer' : '';
+});
+// Close the popup when the map is moved
+map.on('movestart', disposePopover);
